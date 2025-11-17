c02eeba22c73e439cb4cbb8c7798333c9cb32ad9
Nils Georg Skutle <nilsgs@gmail.com>
2025-11-17 09:52:43 +0100

    Reorganize profile.ps1 and add optional comments
    Reordered sections for better organization and added optional comments.

--- oh-my-posh/profile.ps1
+++ oh-my-posh/profile.ps1
@@ -2,13 +2,15 @@
 Import-Module -Name Terminal-Icons
 Import-Module posh-git
 
-# Oh-My-Posh
-oh-my-posh init pwsh --config "$env:UserProfile/Code/oh-my-posh/clash.omp.json" | Invoke-Expression
-Enable-PoshTransientPrompt
-
 # PoshGit Settings
 $GitPromptSettings.EnableFileStatus = $false
 
-# PSReadLine Settings
+# Oh-My-Posh
+oh-my-posh init pwsh --config "$env:UserProfile/Code/oh-my-posh/clash.omp.json" | Invoke-Expression
+Enable-PoshTransientPrompt # OPTIONAL
+
+
+
+# PSReadLine Settings OPTIONAL
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler –Key DownArrow -Function HistorySearchForward.
